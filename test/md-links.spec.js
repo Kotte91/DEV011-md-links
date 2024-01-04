a556f42d60867b44c42a56f9a18dc8bfa36bee4e
Kotte91 <kottevaldebenito@gmail.com>
2024-01-04 15:15:11 -0300

    avance-04/01/24

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,4 +1,4 @@
-const { existRoute, readFile, isAbsolutePath, convertAbsolute, extensionName, nameExt } = require('../src/function');
+const { existRoute, readFile, isAbsolutePath, convertAbsolute, extensionName, nameExt, extractLinks, statsLinks } = require('../src/function');
 
 describe('function', () => {
    it('deberia se una ruta parcial', () => {
@@ -29,11 +29,42 @@ describe('function', () => {
     expect(result instanceof Promise).toBe(true);
     expect(typeof result.then).toBe('function');
   });
-  it('esta funcion nos da una promesa', () => {
+  it('readFile se rechaza correctamente para un archivo inexistente', async () => {
+    await expect(readFile('/ruta/inexistente.txt')).rejects.toMatch('Hay un error con la lectura del archivo');
+  });
+  
+  it('esta en una funcion', () => {
     expect(typeof extensionName).toBe('function')
   })
-  it('esta funcion nos da una promesa', () => {
+  it('esta en una funcion', () => {
     expect(typeof nameExt).toBe('function')
   })
+  it('retorna true para una ruta con extencion valida', ()=>{
+    const resultado = nameExt('/ruta/al/archivo.md');
+    expect(resultado).toBe(true)
+  })
    
+  it('retorna "archivo Invalido" para una ruta con extencion no valida', ()=>{
+    const resultado = nameExt('/ruta/al/archivo.txt');
+    expect(resultado).toBe('archivo inválido')
+  })
+  it('Retorna "archivo inválido" para una ruta sin extensión', () => {
+    const result = nameExt('/archivo_sin_extension');
+    expect(result).toBe('archivo inválido');
+  });
+  it('extractLinks retorna un array de objetos para datos HTML válidos', () => {
+    const data = '<a href="https://ejemplo.com/">Enlace de ejemplo</a>';
+    const file = '/src/prueba.md';
+    const result = extractLinks(data, file);
+    expect(result).toEqual([{ href: 'https://ejemplo.com/', text: 'Enlace de ejemplo', file }]);
+  });
+  
+  it('statsLinks devuelve estadísticas correctas para array de enlaces sin validación', async () => {
+    const arrObjs = [
+      { href: 'https://ejemplo1.com', text: 'Enlace 1', file: '/ruta/al/archivo.md' },
+      { href: 'https://ejemplo2.com', text: 'Enlace 2', file: '/ruta/al/archivo.md' },
+    ];
+    const result = await statsLinks(arrObjs, false);
+    expect(result).toBe('Total: 2\nUnique: 2');
+  });
 });
